2da63b8fa61bc26be31f2b604506e42bbf57ac61
Erzete <82033253+Erzete@users.noreply.github.com>
2022-11-16 23:44:32 +0700

    Tutor 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'form.dart';
+import 'package:lab6/page/form.dart';
+import 'package:lab6/page/to_do_page.dart';
 
 
 void main() {
@@ -78,6 +79,16 @@ class _MyHomePageState extends State<MyHomePage> {
                   Navigator.pushReplacement(
                     context,
                     MaterialPageRoute(builder: (context) => const MyFormPage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text('To Do'),
+                onTap: () {
+                  // Route menu ke halaman to do
+                  Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ToDoPage()),
                   );
                 },
               ),
